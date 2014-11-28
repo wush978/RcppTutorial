e92ce35d8c242467a7aa8b90ad5773d5510baf04
Wush Wu <wush978@gmail.com>
2014-11-28 21:10:09 +0800

    exercise2

---
+++ inst/exercise/exercise2.cpp
@@ -0,0 +1,19 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+
+/**
+ * Please implement the following function that
+ * after `courceCpp`, you can get the mean of 
+ * the first column of iris via:
+ * 
+ * ```
+ * > firstColumnMean(iris)
+ * [1] 5.843333
+ * ```
+ * 
+ * Hint: data.frame is list!
+ */
+// [[Rcpp::export]]
+double firstColumnMean(List iris) {
+  // ...
+}
